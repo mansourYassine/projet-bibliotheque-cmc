011a5df80d8ca8972ae81f0e6eeea721c9c1439e
badrriad <badrriad2007@gmail.com>
2026-03-16 17:08:17 +0000

    update class livre

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ class livre:
         self.statut=statut
     def afficher_details(self):
         return f"titre={self.titre} auteur={self.auteur} pages={self.nombre_page} statut={self.statut}"
+    def emprunter(self):
+        if self.statut=="disponible":
+            self.statut="emprunté"
+            print("le livre est emprunté")
+        else:
+            print("le livre est deja emprunté")
+    def rendre(self):
+        self.statut="disponible"
+        print("le livre est maintenent disponible")
 
         
