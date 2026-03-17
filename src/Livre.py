f1b8cea14482bde5f3c9279440123c8e1325440e
yassine mansour <yassineaitman@gmail.com>
2026-03-17 18:02:59 +0000

    Update the folders structure

---
+++ src/Livre.py
@@ -0,0 +1,21 @@
+class Livre:
+    nombre_livre=0
+    def __init__(self,titre,auteur,nbp,statut="disponible"):
+        self.nombre_livre+=1
+        self.id_livre=self.nombre_livre
+        self.titre=titre
+        self.auteur=auteur
+        self.nombre_page=nbp
+        self.statut=statut
+
+    def afficher_details(self):
+        return f"titre : {self.titre} | auteur : {self.auteur} | pages : {self.nombre_page} | statut : {self.statut}"
+
+    def emprunter(self):
+        if self.statut=="disponible":
+            self.statut="emprunté"
+            return "livre emprunté"
+
+    def rendre(self):
+        self.statut="disponible"
+        return "livre rendu"
