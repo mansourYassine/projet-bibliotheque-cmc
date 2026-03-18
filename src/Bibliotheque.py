921f7a109f1d8d19706c5417d1d2050e36a09e7d
yassine mansour <yassineaitman@gmail.com>
2026-03-18 23:33:17 +0000

    Feature: ajouter nouveau livre

--- src/Bibliotheque.py
+++ src/Bibliotheque.py
@@ -1,19 +1,41 @@
+import csv
+from .Livre import Livre
+
+# from Livre import *
 class Bibliotheque:
     def __init__(self,nom,adresse):
         self.nom=nom
         self.adresse=adresse
-        self.listeLivre=[]
+        self.listeLivres=[]
+        
+    def charger_livre(self):
+        with open('liste_livres.csv', mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                livre = Livre(row['titre'] ,row['auteur'], row['nbre_page'], row['statut'])
+                self.listeLivres.append(livre)
 
-    def ajouter_livre(self,livre):
-        if livre not in self.listeLivre:
-            self.listeLivre.append(livre)
+    def ajouter_livre(self, livre):
+        self.listeLivres.append(livre)
+        
+        with open('liste_livres.csv', mode='a', newline='') as file:
+            fieldnames = ['titre', 'auteur', 'nbre_page', 'statut']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writerow({
+                'titre': livre.titre,
+                'auteur': livre.auteur,
+                'nbre_page' : livre.nombre_page,
+                'statut' : livre.statut
+            })
 
     def afficher_livre_disponible(self):
-        for livre in self.listeLivre:
+        livres_dispo = []
+        for livre in self.listeLivres:
             if livre.statut=="disponible":
-                return livre.titre
+                livres_dispo.append(livre)
+        return livres_dispo
 
     def rechercher_livre(self,titre):
-        for livre in self.listeLivre:
+        for livre in self.listeLivres:
             if livre.titre==titre:
                 return livre
--- src/Livre.py
+++ src/Livre.py
@@ -1,15 +1,12 @@
 class Livre:
-    nombre_livre=0
     def __init__(self,titre,auteur,nbp,statut="disponible"):
-        Livre.nombre_livre += 1
-        self.id_livre= Livre.nombre_livre
         self.titre=titre
         self.auteur=auteur
         self.nombre_page=nbp
         self.statut=statut
 
     def afficher_details(self):
-        return f"id : {self.id_livre} | titre : {self.titre} | auteur : {self.auteur} | pages : {self.nombre_page} | statut : {self.statut}"
+        return f"titre : {self.titre} | auteur : {self.auteur} | pages : {self.nombre_page} | statut : {self.statut}"
 
     def emprunter(self):
         if self.statut=="disponible":
